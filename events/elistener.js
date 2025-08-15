b0095333007f7fd777916c01450f6be99cacb0ad
gauravsandhan01 <gaurav.sandhan01@gmial.com>
2025-08-15 21:52:10 +0530

    event

---
+++ events/elistener.js
@@ -0,0 +1,22 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.addEventListener("click", (evt)=>{
+    console.log("button1 was clicked -handler 1")
+});
+
+btn1.addEventListener("click", (evt)=>{
+    console.log("button1 was clicked -handler 2")
+});
+
+
+const handler3 =()=>{
+    console.log("button1 was clicked -handler 3")
+};
+
+btn1.addEventListener("click", handler3);
+
+btn1.addEventListener("click", (evt)=>{
+    console.log("button1 was clicked -handler 4")
+});
+
+btn1.removeEventListener("click",handler3)
